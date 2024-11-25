bc8c231e84b24e5b4d511e9c37d7c19ec84b6b86
Crazygiscool <huxiaodou768@gmail.com>
2024-11-25 02:46:18 +0000

    installed stuffs, added eaglerxbunge stfu

--- waterfall.sh
+++ waterfall.sh
@@ -2,7 +2,7 @@
 cd waterfall
 wget -O waterfall.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
 while [ true ]; do
-    java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar waterfall.jar --nogui
+    java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -Deaglerxbungee.stfu=true -jar waterfall.jar --nogui
 
     echo Server restarting...
     echo Press CTRL + C to stop.
